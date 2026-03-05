af80f654c1808ac1b41dc09d0f44068900ef1009
Newt Louis <l.ngoc.17395@gmail.com>
2026-03-05 22:53:53 +0700

    add enviroment cors_origin

--- core/settings.py
+++ core/settings.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
+from typing import List
 
-# Tải các biến môi trường từ file .env lên bộ nhớ
+from pydantic import field_validator
+
 load_dotenv()
 
-
 class Settings:
+    CORS_ORIGINS: List[str] = []
     OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
@@ -13,5 +15,12 @@ class Settings:
 
     MAX_CONCURRENT_REQUESTS = int(os.getenv("MAX_CONCURRENT_REQUESTS", 5))
     TIMEOUT_SECONDS = int(os.getenv("TIMEOUT_SECONDS", 60))
+
+    @field_validator("CORS_ORIGINS",mode="before")
+    @classmethod
+    def parse_cors_origins(cls, value):
+        if isinstance(value, str):
+            return [origin.strip() for origin in value.split(",")]
+        return value
 
 settings = Settings()
